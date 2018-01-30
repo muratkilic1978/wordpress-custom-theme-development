e83ccabe6c74db0c758c3618b656d50a96ea4835
muratkilic1978 <muratkilic1978@gmail.com>
2018-01-31 00:31:31 +0100

    functions.php and index.php files were updated

--- wp-content/themes/university-theme/functions.php
+++ wp-content/themes/university-theme/functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function university_files() {
-    wp_enqueue_style('university_main_styles',get_stylesheet_uri());
+
+   wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); wp_enqueue_style('university_main_styles',get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts','university_files');
