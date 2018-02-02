27d1f30c80ecf6ab888d07caa4da84eba9e8e3db
muratkilic1978 <muratkilic1978@gmail.com>
2018-02-02 21:00:56 +0100

    functions.php updated

--- wp-content/themes/university-theme/functions.php
+++ wp-content/themes/university-theme/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function university_files() {
+    \# Load custom Google fonts into WordPress Custom Theme
+    wp_enqueue_style('custom-google-font', "https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     \# Load main stylesheet into WordPress Custom Theme
     wp_enqueue_style('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'); 
     \# Load font-awesome icons into WordPress Custom Theme
